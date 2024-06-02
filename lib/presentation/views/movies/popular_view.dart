e7e7a89510703b310b7dafd9874213c59e31032a
Ridley <gonzalezfranks@hotmail.com>
2024-06-02 17:57:47 +0200

    Fix: Sección categorias

--- lib/presentation/views/movies/popular_view.dart
+++ lib/presentation/views/movies/popular_view.dart
@@ -4,7 +4,7 @@ import 'package:cinemapedia/presentation/widgets/movies/movie_masonry.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:flutter/material.dart';
+
 
 class PopularView extends ConsumerWidget {
   const PopularView({super.key});
@@ -21,39 +21,9 @@ class PopularView extends ConsumerWidget {
     return Scaffold(
       body: MovieMasonry(
         loadNextPage: ref.read(popularMoviesProvider.notifier).loadNextPage,
+        //loadNextPage: () => ref.read(popularMoviesProvider.notifier).loadNextPage(),
         movies: popularMovies,
       ),
     );
   }
 }
-
-
-/*
-class PopularView extends ConsumerStatefulWidget {
-  const PopularView({super.key});
-
-  @override
-  PopularViewState createState() => PopularViewState();
-}
-
-class PopularViewState extends ConsumerState<PopularView> {
-
-  @override
-  Widget build(BuildContext context) {
-
-    final List<Movie> popularMovies = ref.watch(popularMoviesProvider);
-
-
-    if(popularMovies.isEmpty){
-      return const Center(child: CircularProgressIndicator(strokeWidth: 2,));
-    }
-
-    return Scaffold(
-      body:MovieMasonry(
-        loadNextPage: () => ref.read(popularMoviesProvider.notifier).loadNextPage(),
-        movies: popularMovies,
-      )
-    );
-  }
-}
-*/
